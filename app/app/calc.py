5d2141679d385fc117e1914eb9ef9df231eca00b
Anilmgr <anilcodes60@gmail.com>
2025-03-01 23:12:06 +1100

    postgresql setup

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+'''
+Calaclator functions
+'''
+
+def add(x, y):
+    '''
+    Add two numbers together
+    '''
+    return x + y
+
+def multiply(x, y):
+    '''
+    Multiply two numbers together
+    '''
+    return x * y
